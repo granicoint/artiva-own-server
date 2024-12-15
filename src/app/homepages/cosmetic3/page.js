0736ef8fbab9f56a6c87a382fdaf67e3a2444dd6
granicoint <granicoint@gmail.com>
2024-12-15 15:08:23 +0530

    homepages/cosmetic3/page.tsx saved

--- src/app/homepages/cosmetic3/page.js
+++ src/app/homepages/cosmetic3/page.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import TopNavOne from '@/components/Header/TopNav/TopNavOne'
-import MenuCosmeticThree from '@/components/Header/Menu/MenuCosmeticThree'
-import SliderCosmeticThree from '@/components/Slider/SliderCosmeticThree'
-import BannerTop from '@/components/Cosmetic3/BannerTop'
-import Banner from '@/components/Cosmetic3/Banner'
-import productData from '@/data/Product.json'
-import HotProduct from '@/components/Cosmetic3/HotProduct'
-import FeaturedProduct from '@/components/Cosmetic3/FeaturedProduct'
-import BuyPack from '@/components/Cosmetic1/BuyPack'
-import AdsPhoto from '@/components/Cosmetic1/AdsPhoto.jsx'
-import BestSellers from '@/components/Cosmetic3/BestSellers'
-import Benefit from '@/components/Cosmetic3/Benefit'
-import VideoTutorial from '@/components/Cosmetic3/VideoTutorial'
-import Newsletter from '@/components/Cosmetic3/Newsletter'
-import Footer from '@/components/Footer/Footer'
-import ModalNewsletter from '@/components/Modal/ModalNewsletter'
+import React from "react";
+import TopNavOne from "@/components/Header/TopNav/TopNavOne";
+import MenuCosmeticThree from "@/components/Header/Menu/MenuCosmeticThree";
+import SliderCosmeticThree from "@/components/Slider/SliderCosmeticThree";
+import BannerTop from "@/components/Cosmetic3/BannerTop";
+import Banner from "@/components/Cosmetic3/Banner";
+import productData from "@/data/Product.json";
+import HotProduct from "@/components/Cosmetic3/HotProduct";
+import FeaturedProduct from "@/components/Cosmetic3/FeaturedProduct";
+import BuyPack from "@/components/Cosmetic1/BuyPack";
+import AdsPhoto from "@/components/Cosmetic1/AdsPhoto.jsx";
+import BestSellers from "@/components/Cosmetic3/BestSellers";
+import Benefit from "@/components/Cosmetic3/Benefit";
+import VideoTutorial from "@/components/Cosmetic3/VideoTutorial";
+import Newsletter from "@/components/Cosmetic3/Newsletter";
+import Footer from "@/components/Footer/Footer";
+import ModalNewsletter from "@/components/Modal/ModalNewsletter";
 
 export default function HomeCosmeticThree() {
     return (
         <>
-            <TopNavOne props="style-one bg-black" slogan='New customers save 10% with the code GET10' />
-            <div id="header" className='relative w-full'>
+            <TopNavOne props="style-one bg-black" slogan="New customers save 10% with the code GET10" />
+            <div id="header" className="relative w-full">
                 <MenuCosmeticThree />
                 <SliderCosmeticThree />
-                <BannerTop props="bg-[#F4C6A5] md:py-8 py-4" textColor='text-black' bgLine='bg-black' />
+                <BannerTop props="bg-[#F4C6A5] md:py-8 py-4" textColor="text-black" bgLine="bg-black" />
             </div>
             <Banner />
             <HotProduct data={productData} start={3} limit={9} />
@@ -37,5 +37,5 @@ export default function HomeCosmeticThree() {
             <Footer />
             <ModalNewsletter />
         </>
-    )
+    );
 }
